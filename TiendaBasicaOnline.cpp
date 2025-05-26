6f5a51d5aafdd3bb03aa811208a720b20e07ec3f
Mesconiji <josemesconi777@gmail.com>
2025-05-25 23:37:10 -0300

    Update TiendaBasicaOnline.cpp
    Agregado de las 2 primeras clases del requerimiento 1 del TP

--- TiendaBasicaOnline.cpp
+++ TiendaBasicaOnline.cpp
@@ -1,1 +1,30 @@
 //Tienda Online
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// --- Declaraciones anticipadas ---
+// Las clases Cliente, Producto, Pedido y Carrito se declaran aquí para que puedan ser referenciadas entre sí.
+class Producto;
+class Pedido;
+
+// --- Clase Producto ---
+// Define los datos básicos de un producto.
+class Producto {
+private:
+    string nombre;
+    double precio;
+    string codigo;
+    int stock;
+};
+
+// --- Clase Pedido ---
+// Define los datos de un pedido realizado.
+class Pedido {
+private:
+    vector<Producto> productosComprados; // Un conjunto de productos.
+    double montoTotal;
+};
+
